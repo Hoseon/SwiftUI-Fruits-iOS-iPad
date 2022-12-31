2d061f0d02249de5ad1a539621af3d0fd365d798
hoseon <chs1025@gmail.com>
2022-12-31 15:51:47 +0900

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by HoSeon Chu on 2022/12/31.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
